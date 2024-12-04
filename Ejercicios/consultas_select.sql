ec761fbd29c4d95510f5341dc6389cd26eedaa4e
Kevin Rivera <kevinriveravergaray@gmail.com>
2024-12-03 19:52:31 -0500

    operadores de comparación

--- Ejercicios/consultas_select.sql
+++ Ejercicios/consultas_select.sql
@@ -40,3 +40,32 @@ SELECT
 FROM personas_naturales
 WHERE apellido_paterno LIKE('%s');
 
+-- Encontrar filas mediante un operador de comparación
+-- Listar los clientes cuyo id sea mayor a 90
+SELECT*FROM clientes
+WHERE id > 90;
+-- Mayor igual a 90
+SELECT*FROM clientes
+WHERE id >= 90;
+
+-- Listar los clientes cuyo id sea menor a 90
+SELECT*FROM clientes
+WHERE id < 90;
+-- Menor igual a 90
+SELECT*FROM clientes
+WHERE id <= 90;
+
+-- listar los clientes cuyo id este entre 90 y 100
+
+SELECT*FROM clientes
+WHERE id>90 AND id < 100;
+
+SELECT*FROM clientes
+WHERE id>=90 AND id <= 100;
+
+SELECT*FROM clientes
+WHERE id BETWEEN 90 AND 100;
+
+--Seleccionar todos los id diferentes de noventa
+SELECT*FROM clientes
+WHERE tipo_persona != 90;
