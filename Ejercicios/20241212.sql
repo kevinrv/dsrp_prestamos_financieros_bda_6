bb4f93698b16f5536c7a13b6af2e6638c052ca3a
Kevin Rivera <kevinriveravergaray@gmail.com>
2024-12-12 20:20:07 -0500

    update

--- Ejercicios/20241212.sql
+++ Ejercicios/20241212.sql
@@ -49,7 +49,7 @@ DROP PROCEDURE sp_kv_insertar_cuotas_prestamo;
 
 SELECT NEWID();
 
--- Evaluando la consulta del procedimeinto
+-- Evaluando la consulta del procedimiento
 	DECLARE @id_prestamo INT
 	SET @id_prestamo=1
 	DECLARE @plazo INT
@@ -76,3 +76,23 @@ SELECT NEWID();
 	FETCH NEXT 1 ROWS ONLY;
 		SET @Counter = @Counter + 1
 	END
+-- Actualizar las cuotas de los prestamos
+DECLARE @num_prestamos INT
+SELECT @num_prestamos=COUNT(*) FROM prestamos;
+DECLARE @Counter1 INT
+	SET @Counter1 = 3
+
+	WHILE @Counter1 <= @num_prestamos
+	BEGIN
+	EXEC dbo.sp_kv_insertar_cuotas_prestamo @Counter1
+	SET @Counter1 = @Counter1 + 1
+	END
+
+--- Update cuotas: Todas las cuotas con fecha anterior a la actual las ponemos en estado Vencido
+
+UPDATE cuotas SET estado='Vencido'
+WHERE fecha_vencimiento<GETDATE();
+
+SELECT*FROM cuotas WHERE estado='Vencido';
+
+-- Alaizando los pagos
